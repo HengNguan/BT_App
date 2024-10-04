2adc77180896254e2d291c68141f1f713a2f877a
skiang9717 <skiang9717@gmail.com>
2024-10-04 14:17:44 +0800

    Restructure

---
+++ lib/Constant/ConstantStyling.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'ConstantString.dart';
+
+class AppTextStyles {
+  static const TextStyle title18Bold = TextStyle(
+      fontFamily: ConstantString.FONT_ADDRESS_SANS_PRO,
+      fontWeight: ConstantString.FONT_WEIGHT_BOLD,
+      fontSize: 18);
+
+  static const TextStyle regular12 = TextStyle(
+      fontFamily: ConstantString.FONT_ADDRESS_SANS_PRO,
+      fontWeight: ConstantString.FONT_WEIGHT_REGULAR,
+      fontSize: 12);
+
+  static const TextStyle bold12 = TextStyle(
+      fontFamily: ConstantString.FONT_ADDRESS_SANS_PRO,
+      fontWeight: ConstantString.FONT_WEIGHT_BOLD,
+      fontSize: 12);
+
+  static const TextStyle grey12Regular = TextStyle(
+      color: Colors.grey,
+      fontFamily: ConstantString.FONT_ADDRESS_SANS_PRO,
+      fontWeight: ConstantString.FONT_WEIGHT_REGULAR,
+      fontSize: 12);
+
+  static const TextStyle grey16Regular = TextStyle(
+      color: Colors.grey,
+      fontFamily: ConstantString.FONT_ADDRESS_SANS_PRO,
+      fontWeight: ConstantString.FONT_WEIGHT_REGULAR,
+      fontSize: 16);
+
+  static const TextStyle regular20 = TextStyle(
+      fontFamily: ConstantString.FONT_ADDRESS_SANS_PRO,
+      fontWeight: ConstantString.FONT_WEIGHT_REGULAR,
+      fontSize: 20);
+
+  static const TextStyle bold40 = TextStyle(
+      fontFamily: ConstantString.FONT_ADDRESS_SANS_PRO,
+      fontWeight: FontWeight.bold,
+      fontSize: 40);
+}
--- lib/generated/intl/messages_zh.dart
+++ lib/generated/intl/messages_zh.dart
@@ -20,14 +20,38 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'zh';
 
+  static String m0(countdown) => "下一个提醒在 ${countdown} 小时后";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "drinkLog": MessageLookupByLibrary.simpleMessage("饮水日志"),
+        "bluetoothIsOff": MessageLookupByLibrary.simpleMessage("蓝牙已关闭或不可用"),
+        "bluetoothIsOn": MessageLookupByLibrary.simpleMessage("蓝牙已开启"),
+        "bluetoothScanner": MessageLookupByLibrary.simpleMessage("蓝牙扫描器"),
+        "bluetooth_home_page": MessageLookupByLibrary.simpleMessage(
+            "==================== 蓝牙首页 ==================== "),
+        "calendar_page": MessageLookupByLibrary.simpleMessage(
+            "==================== 日历页面 ==================== "),
+        "connecting": MessageLookupByLibrary.simpleMessage("正在连接..."),
+        "dataPacketReceived": MessageLookupByLibrary.simpleMessage("已接收到数据包："),
+        "default_page": MessageLookupByLibrary.simpleMessage(
+            "==================== 默认页面 ==================== "),
+        "disconnected": MessageLookupByLibrary.simpleMessage("已断开连接"),
+        "drinkLog": MessageLookupByLibrary.simpleMessage("饮水记录"),
+        "homeTabContent": MessageLookupByLibrary.simpleMessage("首页内容"),
+        "home_page": MessageLookupByLibrary.simpleMessage(
+            "==================== 首页 ==================== "),
+        "nextReminder": m0,
         "noDataAvailable": MessageLookupByLibrary.simpleMessage("没有更多的数据"),
+        "notSupportThisDevice":
+            MessageLookupByLibrary.simpleMessage("此设备不支持蓝牙"),
         "overview": MessageLookupByLibrary.simpleMessage("概览"),
-        "percentGoal": MessageLookupByLibrary.simpleMessage("目标百分比"),
+        "parsedData": MessageLookupByLibrary.simpleMessage("解析数据"),
+        "percentGoal": MessageLookupByLibrary.simpleMessage("目标完成百分比"),
         "progressGoal": MessageLookupByLibrary.simpleMessage("进度 / 目标"),
+        "successfulConnect": MessageLookupByLibrary.simpleMessage("连接成功"),
         "today": MessageLookupByLibrary.simpleMessage("今天"),
+        "todayProgress": MessageLookupByLibrary.simpleMessage("今日进度"),
+        "turnOnBluetooth": MessageLookupByLibrary.simpleMessage("请开启蓝牙"),
         "viewAll": MessageLookupByLibrary.simpleMessage("查看全部")
       };
 }
